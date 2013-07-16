1bf877a07d93cc68fca65e0f99562b75406cf598
Chuka Okoye <okoye9@gmail.com>
2013-07-16 11:39:15 -0700

    finished implementation of statistics parser, now adding implementation for namespaces

--- parsers.py
+++ parsers.py
@@ -1,10 +1,18 @@
 '''
-responsible for parsing raw results returned by aerospike
+abstracts how the data from aerospike is parsed in case of future changes.
+
 all functions either return a dictionary or a generator of tuples (key, value)
 for use by calling function.
+
+aerospike in turn either returns a big string or dictionary.
 '''
 def statistics(raw):
-  #parse and return all values.
+  #since statistics module returns a big a$$ string, return results
+  #immediately (via a generator) as data gets parsed
   stats = raw.split(';')
-  for value in stats:
-    print value.split('=')
+  for metric in stats:
+    key, value = metric.split('=')
+    yield (key, value)
+
+def namespace(raw):
+  #
