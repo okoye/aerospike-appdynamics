20a4bc347830cbaaf99cfa4b1654d955865d1e2e
Chuka Okoye <okoye9@gmail.com>
2013-07-16 11:51:44 -0700

    refactored parsers and added implementation of namespace parsing

--- parsers.py
+++ parsers.py
@@ -6,13 +6,22 @@ for use by calling function.
 
 aerospike in turn either returns a big string or dictionary.
 '''
-def statistics(raw):
-  #since statistics module returns a big a$$ string, return results
-  #immediately (via a generator) as data gets parsed
-  stats = raw.split(';')
+def _parse(delimiter=';'):
+  '''
+  internal routine to parse information and return as it is generated
+  '''
+  stats - raw.split(delimiter)
   for metric in stats:
     key, value = metric.split('=')
     yield (key, value)
 
+def statistics(raw):
+  #since statistics module returns a big a$$ string, return results
+  #immediately (via a generator) as data gets parsed
+  for k, v in _parse(raw):
+    yield k, v
+
 def namespace(raw):
-  #
+  #returns results immediately (via a generator) as data gets parsed
+  for k, v in _parse(raw):
+    yield k, v
