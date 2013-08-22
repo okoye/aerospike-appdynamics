a4da9d4e86ed7bb92efb99796429cdf137acf841
Chuka Okoye <okoye9@gmail.com>
2013-08-22 12:55:21 -0700

    switching to sqlite backend

--- lib/a/wsgc/aerospike/settings.py
+++ lib/a/wsgc/aerospike/settings.py
@@ -4,4 +4,4 @@ settings the python way
 LIB_DIR = '/home/vagrant/citrusleaf_client_swig_2.1.11/swig/python'
 PORT = None #using default 3000
 HOST = None #using current host as default
-DB_FILE = '/tmp/aerospike_appdynamics_store.leveldb'
+DB_FILE = '/tmp/aerospike_appdynamics_store.sqlite'
