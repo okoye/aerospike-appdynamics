b827c78c3dda37f5219e9cd0a3c9d24cc487fb6b
Chuka Okoye <okoye9@gmail.com>
2013-08-29 13:42:08 -0700

    added setup.cfg file

--- setup.py
+++ setup.py
@@ -4,12 +4,9 @@ from setuptools import setup, find_packages
 #http://semver.org
 
 setup(name='aerospike-appdynamics-connector',
-      version='0.1.2',
       description='aerospike abstraction library for appdynamics scripts',
       author='Chuka O',
       author_email='dokoye@wsgc.com',
       package_dir={'':'lib'},
       packages=find_packages(where='lib'),
-      test_suite='tests',
-      maintainer='Chuck Scott',
-      maintainer_email='cscott2@wsgc.com')
+      test_suite='tests')
