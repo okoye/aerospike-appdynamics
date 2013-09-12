e59c5e3eda3d8f59e5de0c01ca3fd6fd23e26f12
Chuka Okoye <okoye9@gmail.com>
2013-09-12 15:59:19 -0700

    merging all the data collection scripts into one

---
+++ apm-monitor-scripts/latency_reads.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from a.wsgc.aerospike.monitor import AerospikeAnalyticsConnector
+
+
+PERFORMANCE_PREFIX = 'Performance'
+UTILIZATION_PREFIX = 'Utilization'
+OPERATIONAL_PREFIX = 'Operation'
+
+def printer(prefix='Aerospike|Generic', name, value):
+  print '%s|%s, value=%s'%(prefix, name, value)
+
+col = AerospikeAnalyticsConnector()
+unknown_errors = col.statistics('stat_read_errs_other')
+uptime = col.statistics('uptime')
+
+printer(OPERATIONAL_PREFIX,'unknown_errors', unknown_errors)
+printer(OPERATIONAL_PREFIX, 'cluster_uptime')
+printer(OPERATIONAL_PREFIX, '')
