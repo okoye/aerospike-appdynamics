d41b025c4c17f1995911a60bd595af7b4bb7acea
Chuka Okoye <okoye9@gmail.com>
2013-10-02 11:20:00 -0700

    switching to internal build

--- apm-monitor-scripts/metrics.py
+++ apm-monitor-scripts/metrics.py
@@ -6,13 +6,27 @@ PERFORMANCE_PREFIX = 'Performance'
 UTILIZATION_PREFIX = 'Utilization'
 OPERATIONAL_PREFIX = 'Operation'
 
-def printer(name, value, prefix='Aerospike|Generic', delta=False):
+def printer(name, value, prefix='Aerospike|Generic'):
   print '%s|%s, value=%s'%(prefix, name, value)
 
 col = AerospikeAnalyticsConnector()
-unknown_errors = col.statistics('stat_read_errs_other')
-uptime = col.statistics('uptime')
 
-printer(OPERATIONAL_PREFIX,'unknown_errors', unknown_errors)
-printer(OPERATIONAL_PREFIX, 'cluster_uptime')
-printer(OPERATIONAL_PREFIX, '')
+######  Unknown Errors  ###########
+unknown_errors = col.statistics('stat_read_errs_other')
+unknown_erros = len(unknown_errors) #TODO: delta changes.
+
+
+##### Uptime  ###################
+uptime = math.fabs(col.statistics('uptime'))
+
+
+####  Cluster Size #############
+cluster_size = col.statistics('cluster_size').get()
+
+
+
+
+
+#printer(OPERATIONAL_PREFIX,'unknown_errors', unknown_errors)
+#printer(OPERATIONAL_PREFIX, 'cluster_uptime')
+#printer(OPERATIONAL_PREFIX, 'stat_read_reqs')
