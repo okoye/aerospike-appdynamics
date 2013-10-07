7f7434298a6aba20e2a1b9e9d411b5ad930bef23
Chuka Okoye <okoye9@gmail.com>
2013-10-07 15:22:32 -0700

    version bump

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ from setuptools import setup, find_packages
 #http://semver.org
 
 setup(name='aerospike-appdynamics-connector',
-      version='0.2.0',
+      version='0.2.2',
       description='aerospike abstraction library for appdynamics scripts',
       author='Chuka O',
-      author_email='dokoye@wsgc.com',
+      author_email='okoye9@gmail.com',
       package_dir={'':'lib'},
       packages=find_packages(where='lib'),
       test_suite='tests')
